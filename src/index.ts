5c4087b71f8b4b04c0e3536f2173c1a2daaeb824
Karol Torun <krol22ee@gmail.com>
2019-02-24 16:58:07 +0100

    Name change, updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
-import { ECS } from './ECS';
+import { EcsSystem } from './EcsSystem';
 
 export * from './Ecs';
 export * from './EcsComponent';
 export * from './EcsEntity';
 export * from './EcsSystem';
-
-let ecs = new ECS();
-
-console.log(ecs);
